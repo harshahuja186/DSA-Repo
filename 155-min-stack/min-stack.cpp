6fa63ae374259b2530d256cc5b758cf4d138d8fd
harshahuja186 <83861192+harshahuja186@users.noreply.github.com>
2023-10-30 19:00:12 +0530

    Time: 21 ms (33.33%) | Memory: 16.7 MB (29.74%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,36 +1,48 @@
-class MinStack {
-    stack<pair<int,int>> st;
-public:
-    MinStack() {
-        
-    }
-    
-    void push(int num) {
-        if(st.empty()) st.push({num,num});
-		else st.push({num,min(st.top().second,num)});
-    }
-    
-    void pop() {
-        if(st.empty()) return;
-        st.pop();
-    }
-    
-    int top() {
-        if(st.empty()) return -1;
-        return st.top().first;   
-    }
-    
-    int getMin() {
-        if(st.empty()) return -1;
-		return st.top().second;
-    }
-};
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack* obj = new MinStack();
- * obj->push(val);
- * obj->pop();
- * int param_3 = obj->top();
- * int param_4 = obj->getMin();
- */
+class MinStack {
+  stack < long long > st;
+  long long mini;
+  public:
+    /** initialize your data structure here. */
+    MinStack() {
+      while (st.empty() == false) st.pop();
+      mini = INT_MAX;
+    }
+
+  void push(int value) {
+    long long val=(long long)value ;
+    if (st.empty()) {
+      mini = val;
+      st.push(val);
+    } else {
+      if (val < mini) {
+        st.push(2 *val*1LL - mini);
+        mini = val;
+      } else {
+        st.push(val);
+      }
+    }
+  }
+
+  void pop() {
+    if (st.empty()) return;
+    long long el = st.top();
+    st.pop();
+
+    if (el < mini) {
+      mini = 2 * mini - el;
+    }
+  }
+
+  int top() {
+    if (st.empty()) return -1;
+
+    long long el = st.top();
+    if (el < mini) return mini;
+    return el;
+  }
+
+  int getMin() {
+    return mini;
+  }
+};
