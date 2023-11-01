2fba1c4af81dc79c6d2fc4d2975eec729670b028
harshahuja186 <83861192+harshahuja186@users.noreply.github.com>
2023-11-01 18:57:28 +0530

    Time: 81 ms (46.40%) | Memory: 46.3 MB (10.76%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,48 @@
+class Solution {
+    vector<int> nsl_indices(vector<int> arr, int n){
+        vector<int> nsl(n,-1);
+        stack<int> st;
+
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            if(!st.empty()) nsl[i] = st.top();
+            st.push(i);
+        }
+        return nsl;
+    }
+
+
+    vector<int> nsr_indices(vector<int> arr, int n){
+        stack<int> st;
+        vector<int> nsr(n,n);
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(!st.empty()) nsr[i] = st.top();
+            st.push(i);
+        }
+        return nsr;
+    }    
+public:
+    int sumSubarrayMins(vector<int>&arr) {
+        long sum = 0;
+        int n = arr.size();
+        int MOD = 1e9 + 7;
+
+        vector<int> nsl = nsl_indices(arr, n);
+        vector<int> nsr = nsr_indices(arr, n);
+
+        for(int i = 0 ; i < n; i++){
+            int ls = i-nsl[i];
+            int rs = nsr[i]-i;
+
+            int totol_ways = ls * rs;
+            sum = (sum + (static_cast<long>(arr[i]) * totol_ways))%MOD;
+        }
+        return sum;
+    }
+};
